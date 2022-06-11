18498a70c5dbcace30dcda48abbb20e970882871
Vladislav <vladveprev29@gmail.com>
2022-06-11 13:34:02 +0300

    removed unnecessary files

--- scripts/PCA_model.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-from sklearn.preprocessing import MinMaxScaler, StandardScaler
-from sklearn.decomposition import PCA
-from sklearn.manifold import TSNE
-
-
-
